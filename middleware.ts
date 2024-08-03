795738cb4886829a161459813f8d5427255dd296
awelrisak <abdurazak.awil1@gmail.com>
2024-08-03 15:07:12 +0300

    Update: updates csp

--- middleware.ts
+++ middleware.ts
@@ -30,18 +30,19 @@ export const config = {
 function csp(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
-     default-src 'self';
-     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-     style-src 'self' 'nonce-${nonce}';
-     img-src 'self' blob: data:;
-     font-src 'self';
-     object-src 'none';
-     base-uri 'self';
-     form-action 'self';
-     frame-ancestors 'none';
-     block-all-mixed-content;
-     upgrade-insecure-requests;
- `;
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: 'unsafe-inline' ${
+      process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
+    };
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
   // Replace newline characters and spaces
   const contentSecurityPolicyHeaderValue = cspHeader
     .replace(/\s{2,}/g, " ")
