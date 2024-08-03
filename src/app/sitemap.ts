da28f95bc8f81c4a4e568ed9bf37c448285f762a
awelrisak <abdurazak.awil1@gmail.com>
2024-08-03 14:18:31 +0300

    Fix: makes sitemap url's to absolute

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,10 +5,8 @@ import { SitemapQueryResult } from "@/sanity/sanity.types";
 import type { MetadataRoute } from "next";
 
 const mainNavigations: MetadataRoute.Sitemap = mainNav.map((nav) => ({
-  url: nav.href,
+  url: `${siteConfig.url}/${nav.href}`,
   lastModified: new Date(),
-  changeFrequency: "weekly",
-  priority: 1,
 }));
 
 export default async function sitemap() {
@@ -17,8 +15,6 @@ export default async function sitemap() {
   const blog = siteMapData.blog.map((post) => ({
     url: `${siteConfig.url}/blog/${post.slug}`,
     lastModified: post.publishedAt,
-    changeFrequency: "weekly",
-    priority: 1,
   }));
   const services = siteMapData.services.map((service) => ({
     url: `${siteConfig.url}/services/${service.slug}`,
