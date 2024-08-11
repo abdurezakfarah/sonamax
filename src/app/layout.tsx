11bf8ef91388481605984785726e71572467a399
awelrisak <abdurazak.awil1@gmail.com>
2024-08-11 15:21:43 +0300

    Update: move from no bg favicon images to one with black bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,13 +57,38 @@ export const metadata: Metadata = {
   icons: [
     {
       rel: "icon",
-      url: "/favicon/favicon-32x32.png",
-      sizes: "32x32",
+      url: "/favicon/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon/android-chrome-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
     },
     {
       rel: "icon",
       url: "/favicon/favicon-16x16.png",
       sizes: "16x16",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon/favicon-48x48.png",
+      sizes: "48x48",
+      type: "image/png",
+    },
+    {
+      rel: "apple-touch-icon",
+      url: "/favicon/apple-touch-icon.png",
+      sizes: "180x180",
     },
     {
       rel: "mask-icon",
@@ -74,7 +99,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#fa4c29",
+  themeColor: "#000000",
 };
 
 export default function RootLayout({
